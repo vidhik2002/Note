3df598a1612fedadd7b316d215e5a6eb09a9fcba
Vidhi Khaitan <vidhik2002@gmail.com>
2021-07-10 11:41:27 +0530

    feat: add note styles

--- src/components/addNote.js
+++ src/components/addNote.js
@@ -1,7 +1,7 @@
 const Add = () => {
     return (
       <div className="new">
-        <textarea rows="8" columns="10" placeholder="Type note here....."></textarea>
+        <textarea rows="8" columns="10" placeholder="Type note here....." className="type" ></textarea>
         <div className="bottom">
             <span className="wordleft">200 remaining</span>
             <button className="save">Save</button> 
