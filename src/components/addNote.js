475556c356d2ce61eb8a94e1221b6a4c731f9000
Vidhi Khaitan <vidhik2002@gmail.com>
2021-07-11 12:06:59 +0530

    feat: word limit

--- src/components/addNote.js
+++ src/components/addNote.js
@@ -2,9 +2,12 @@ import { useState } from "react";
 
 const Add = ({handlesaveNote}) => {
     const[addnote, setaddnote] = useState('')//text of the note
+    const characterLimit = 10
 
     const Change = (e) => {
+        if(characterLimit - e.target.value.length >= 0 ){
         setaddnote(e.target.value)
+        }
     }
     const saveClick = () => {
         if(addnote.trim().length>0){
@@ -17,7 +20,7 @@ const Add = ({handlesaveNote}) => {
       <div className="new">
         <textarea rows="8" columns="10" placeholder="Type note here....." className="type" value={addnote} onChange={Change} ></textarea>
         <div className="bottom">
-            <span className="wordleft">200 remaining</span>
+            <span className="wordleft">{characterLimit-addnote.length} remaining</span>
             <button className="save" onClick={saveClick}>Save</button> 
         </div>
       </div>
